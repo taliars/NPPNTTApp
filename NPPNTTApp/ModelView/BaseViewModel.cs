9f3376c30cb36806af78641692fbe187bc3bcdd8
Таляр Станислав Михайлович <tsm@bkn-spb.ru>
2019-01-25 14:22:01 +0300

    Move INotifyPropertyChanged to base class

---
+++ NPPNTTApp/ModelView/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace NPPNTTApp.ModelView
+{
+    public class BaseViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
