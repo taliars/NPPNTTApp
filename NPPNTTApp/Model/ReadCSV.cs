bf7cf55297e22353b7049d9e8b0ba3dbbe031fc7
Таляр Станислав Михайлович <tsm@bkn-spb.ru>
2019-01-25 16:54:57 +0300

    Add load cancellation, edit xaml styling

--- NPPNTTApp/Model/ReadCSV.cs
+++ NPPNTTApp/Model/ReadCSV.cs
@@ -11,46 +11,11 @@ namespace NPPNTTApp.Model
 {
     public static class ReadCSV
     {
-        public static  List<BaseClass> ReadFile()
-        {
-            List<BaseClass> outPutList = new List<BaseClass>();
-
-            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            dialog.Filters.Add(new CommonFileDialogFilter("csv файлы", "*.csv"));
-
-            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                string[] lines = File.ReadAllLines(dialog.FileName);
-
-                var data = from l in lines.Skip(1)
-                            let split = l.Split(';')
-                             select new BaseClass
-                             {
-                                 Date = DateTime.Parse(split[0]),
-                                 ObjectA = split[1],
-                                 TypeA = split[2],
-                                 ObjectB = split[3],
-                                 TypeB = split[4],
-                                 Direction = (Direction)Enum.Parse(typeof(Direction), split[5]),
-                                 Color = split[6],
-                                 Intensity = int.Parse(split[7]),
-                                 LatitudeA = double.Parse(split[8], CultureInfo.InvariantCulture),
-                                 LongitudeA = double.Parse(split[9], CultureInfo.InvariantCulture),
-                                 LatitudeB = double.Parse(split[10], CultureInfo.InvariantCulture),
-                                 LongitudeB = double.Parse(split[11], CultureInfo.InvariantCulture),
-                             };
-
-                outPutList = data.ToList();
-            }
-
-            return outPutList;
-        }
-
         public static async Task<List<BaseClass>> Get(
             IProgress<BaseProgressData> progress, 
-            CancellationToken cancellationToken = new CancellationToken())
+            CancellationToken cancellationToken)
         {
-            cancellationToken.ThrowIfCancellationRequested();
+
 
             List<BaseClass> outPutList = new List<BaseClass>();
 
@@ -66,10 +31,16 @@ namespace NPPNTTApp.Model
 
                     string[] some = linesa.Skip(1).ToArray();
 
+                    int i = 0;
+                    int total = some.Length - 1;
+
                     List<BaseClass> data = new List<BaseClass>();
 
                     foreach (var item in some)
                     {
+                        if (cancellationToken.IsCancellationRequested)                        
+                            return outPutList;       
+
                         if (string.IsNullOrEmpty(item)) break;
                         
                         string[] split = item.Split(';');
@@ -91,37 +62,19 @@ namespace NPPNTTApp.Model
                         };
 
                         data.Add(baseClass);
-                    }
 
-                    //var data = from l in linesa.Skip(1)
-                    //           let split = l.Split(';')
-                    //           select new BaseClass
-                    //           {
-                    //               //Date = DateTime.Parse(split[0]),
-                    //               ObjectA = split[1],
-                    //               TypeA = split[2],
-                    //               ObjectB = split[3],
-                    //               TypeB = split[4],
-                    //               Direction = (Direction)Enum.Parse(typeof(Direction), split[5]),
-                    //               Color = split[6],
-                    //               Intensity = int.Parse(split[7]),
-                    //               LatitudeA = double.Parse(split[8], CultureInfo.InvariantCulture),
-                    //               LongitudeA = double.Parse(split[9], CultureInfo.InvariantCulture),
-                    //               LatitudeB = double.Parse(split[10], CultureInfo.InvariantCulture),
-                    //               LongitudeB = double.Parse(split[11], CultureInfo.InvariantCulture),
-                    //           };
+                        progress?.Report(new BaseProgressData(i + 1, total));
+                        i++;
 
-                    for (int i = 0; i < data.Count(); i++)
-                    {
-                        progress?.Report(new BaseProgressData(i + 1, data.Count()));
-                        await Task.Delay(900);
-                    }
+                        await Task.Delay(1000);
+                    }                
 
                     outPutList = data.ToList();
                 }
             }
 
             return outPutList;
+
         }
     }
 }
--- NPPNTTApp/ModelView/MainViewModel.cs
+++ NPPNTTApp/ModelView/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using NPPNTTApp.Model;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Windows.Input;
 
 namespace NPPNTTApp.ModelView
@@ -8,48 +9,69 @@ namespace NPPNTTApp.ModelView
     public class MainViewModel : BaseViewModel
     {
         private string _progressMessage;
-
+        Progress<BaseProgressData> _progress;
+        private List<BaseClass> _baseClassList;
+        
         public string ProgressMessage
         {
             get { return _progressMessage; }
             set
             {
-                _progressMessage = value;
-                OnPropertyChanged(nameof(ProgressMessage));
+                if (_progressMessage != value)
+                {
+                    _progressMessage = value;
+                    OnPropertyChanged(nameof(ProgressMessage));
+                }
             }
         }
-
-        Progress<BaseProgressData> _progress;
-
-        private List<BaseClass> _baseClassList;
 
         public List<BaseClass> BaseClassList
         {
             get { return _baseClassList; }
             set
             {
-                _baseClassList = value;
-                OnPropertyChanged(nameof(BaseClassList));
+                if (_baseClassList != value)
+                {
+                    _baseClassList = value;
+                    OnPropertyChanged(nameof(BaseClassList));
+                }
             }
         }
 
         public ICommand PopulateTableCommand { get; set; }
         public ICommand ClearTableCommand { get; set; }
         public ICommand CloseAppCommand { get; set; }
+        public ICommand CancelLoadCommand { get; set; }
 
         public MainViewModel()
         {
-            PopulateTableCommand = new RelayCommand
-                (async o => BaseClassList = await ReadCSV.Get(_progress));
+            _progress = new Progress<BaseProgressData>
+                (d => ProgressMessage = $"{d.Item} rows out of {d.Total} is loaded");
 
-            ClearTableCommand = new RelayCommand
-                 (o => BaseClassList = null);
-
+            PopulateTableCommand = new RelayCommand(o => BeginLoad());
+            ClearTableCommand = new RelayCommand(o => BaseClassList = null);
             CloseAppCommand = new RelayCommand(o => OnClosingRequest());
-
-            _progress = new Progress<BaseProgressData>(d => ProgressMessage = $"{d.Item} from {d.Total}");
+            CancelLoadCommand = new RelayCommand(o => CancelLoad());
         }
 
+        CancellationTokenSource m_source;
+
+        private async void BeginLoadAsync(CancellationToken token)
+        {
+            BaseClassList = await ReadCSV.Get(_progress, token);
+        }
+
+        void BeginLoad()
+        {
+            m_source = new CancellationTokenSource();
+            BeginLoadAsync(m_source.Token);
+        }
+
+        private void CancelLoad()
+        {
+            m_source.Cancel();
+            ProgressMessage = "Loading is cancelled";
+        }
 
         protected void OnClosingRequest()
         {
