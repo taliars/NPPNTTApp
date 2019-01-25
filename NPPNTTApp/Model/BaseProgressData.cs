d463eef4c833fa949f598133f7a73f1599dde95f
Таляр Станислав Михайлович <tsm@bkn-spb.ru>
2019-01-25 14:20:14 +0300

    Added

---
+++ NPPNTTApp/Model/BaseProgressData.cs
@@ -0,0 +1,14 @@
+﻿namespace NPPNTTApp.Model
+{
+    public class BaseProgressData
+    {
+        public int Item { get; }
+        public int Total { get; }
+
+        public BaseProgressData(int item, int total)
+        {
+            Item = item;
+            Total = total;
+        }
+    }
+}
